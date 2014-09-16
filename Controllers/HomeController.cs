af6b2d7b53d3a8c7da282106fb3048ce7624c93b
ardour27360 <ardour27360@gmail.com>
2014-09-16 23:13:21 +0400

    First step of invite

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RvspForm() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RvspForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+            
+        }
+
+    }
+}
